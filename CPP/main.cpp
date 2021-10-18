3e6ff235f9014be325a50ee019798e762583f1a1
lovedanu90 <92674086+lovedanu90@users.noreply.github.com>
2021-10-18 09:41:16 +0530

    Add files via upload

---
+++ CPP/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // to  make a array
+    int marks[]={10,20,30,40,50,60,70,90};
+    cout<<marks[0]<<endl;
+    cout<<marks[1]<<endl;
+    cout<<marks[2]<<endl;
+    cout<<marks[3]<<endl;
+    cout<<marks[4]<<endl;
+    
+    //to insert a value in a array
+    int marksw[4];
+    marksw[0] = 1;
+    marksw[1] = 2;
+    marksw[2] = 3;
+    marksw[3] = 4;
+    cout<<marksw[0]<<endl;
+    cout<<marksw[1]<<endl;
+    cout<<marksw[2]<<endl;
+    cout<<marksw[3]<<endl;
+    return 0;
+}
+
