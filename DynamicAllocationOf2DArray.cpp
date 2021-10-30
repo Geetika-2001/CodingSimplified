1e74cd507b2d8fad2ac7e1f76bfea410de75a261
Bhavy Bansal <76460299+BhavyBansal24@users.noreply.github.com>
2021-10-30 19:07:13 +0530

    DynamicAllocationOf2Darray.cpp
    DynamicAllocationOf2Darray

---
+++ DynamicAllocationOf2DArray.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std; 
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+    int** p = new int*[m];
+    for (int i = 0; i < m; i++) {
+        p[i] = new int[n];
+        for (int j = 0; j <n; j++) {
+            cin >> p[i][j];
+        }
+    }
+    for (int i = 0; i<m; i++) {
+        delete [] p[i];
+    }
+    delete [] p;
+}
