8d473fcfc786c155c7c1b0286702d7f8425563f9
UTKARSH SHUKLA <65393419+us25999@users.noreply.github.com>
2021-10-31 19:59:35 +0530

    majority element by MOORE'S VOTING Algorithm
    Cpp code to find the index of the majority element, if present in the array. Majority element is the element which appears more than (n/2) times in an array, where n is the size of the array.

---
+++ CPP/majority_element.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int findMajority(int arr[], int n)
+{
+	int res = 0, count = 1;
+
+    	for(int i = 1; i < n; i++)
+    	{
+    		if(arr[res] == arr[i])
+    			count++;
+    		else 
+    			count --;
+
+    		if(count == 0)
+    		{
+    			res = i; count = 1;
+    		}
+    	}
+
+    	count = 0;
+
+    	for(int i = 0; i < n; i++)
+    		if(arr[res] == arr[i])
+    			count++;
+
+    	if(count <= n /2)
+    		res = -1;
+
+    	return res; 
+}
